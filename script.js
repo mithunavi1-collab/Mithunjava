4b301876ac351fce7b883e69f1794d1c6b542145
mithunavi1-collab <mithunavi1@gmail.com>
2026-08-28 00:11:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let studentName = "Mithun";
+let age = 26;
+let mark = 87;
+
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Mark:", mark);
+
+let userName = prompt("Enter your name:");
+alert("Hello, " + userName);
+
+
+let userAge = prompt("Enter your age:");
+console.log("User Age:", userAge);
+
+
+let answer = confirm("Do you like JavaScript?");
+console.log("User selected:", answer);
+
+
+let name = prompt("Enter your name:");
+document.writeln("Your name is: " + name);
+
